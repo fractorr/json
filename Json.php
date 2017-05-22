25eafbcf0c315b340242935c1c5cbfe11ee3dba9
Trevor Orr <fractorr@gmail.com>
2017-05-22 11:58:30 -0700

    Initial File Commit

---
+++ Json.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Jsondecode plugin for Craft CMS 3.x
+ *
+ * @link      https://fractorr.com/
+ * @copyright Copyright (c) 2017 fractorr
+ * @license   MIT License https://opensource.org/licenses/MIT
+ */
+
+namespace fractorr\json;
+
+use fractorr\json\services\JsonService;
+use fractorr\json\twigextensions\JsonTwigExtension;
+
+use Craft;
+use craft\base\Plugin;
+
+/**
+ * Class Jsondecode
+ *
+ * @author    fractorr
+ * @package   Jsondecode
+ * @since     1.1.0
+ *
+ * @property  JsondecodeService    jsondecode
+ */
+class Json extends Plugin
+{
+    /**
+     * @var Jsondecode
+     */
+    public static $plugin;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        self::$plugin = $this;
+        $this->name = $this->getName();
+
+        // Add in our Twig extensions
+        Craft::$app->view->twig->addExtension(new JsondecodeTwigExtension());
+
+        Craft::info(
+            Craft::t(
+                'json',
+                '{name} plugin loaded',
+                ['name' => $this->name]
+            ),
+            __METHOD__
+        );
+    }
+
+    /**
+     * Returns the user-facing name of the plugin, which can override the name
+     * in composer.json
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return Craft::t('json', 'Json');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function defineTemplateComponent()
+    {
+        return JsonVariable::class;
+    }
+}
